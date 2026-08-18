d5ee3c34db65fc9d4c21534005777c05f6554285
Vivek <268113948+VivekVRobotics@users.noreply.github.com>
2026-08-18 18:49:45 +0530

    Add industrial vision inspection public API

---
+++ src/vision_inspection/__init__.py
@@ -0,0 +1,5 @@
+"""Classical computer-vision inspection primitives."""
+
+from .inspector import InspectionConfig, InspectionResult, inspect_image
+
+__all__ = ["InspectionConfig", "InspectionResult", "inspect_image"]
