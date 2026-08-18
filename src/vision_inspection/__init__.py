c6524114efcdd57503e3c4e9330ffdb68fceb656
Vivek <268113948+VivekVRobotics@users.noreply.github.com>
2026-08-18 19:21:18 +0530

    Expose registration and golden-reference APIs

--- src/vision_inspection/__init__.py
+++ src/vision_inspection/__init__.py
@@ -6,22 +6,12 @@ from .inspector import Defect, InspectionConfig, InspectionResult, inspect_array
 from .metrology import RegionMeasurement, measure_regions
 from .preprocessing import PreprocessConfig
 from .quality import ImageQuality, QualityConfig, assess_image_quality
+from .reference import ReferenceConfig, ReferenceResidual, compare_to_reference
+from .registration import RegistrationResult, rectified_perspective, register_ecc, rectify_perspective
 
 __all__ = [
-    "BinaryConfusion",
-    "CameraCalibration",
-    "Defect",
-    "ImageQuality",
-    "InspectionConfig",
-    "InspectionResult",
-    "PixelScale",
-    "PreprocessConfig",
-    "QualityConfig",
-    "RegionMeasurement",
-    "assess_image_quality",
-    "calibrate_camera",
-    "confusion_from_labels",
-    "inspect_array",
-    "inspect_image",
-    "measure_regions",
+    "BinaryConfusion", "CameraCalibration", "Defect", "ImageQuality", "InspectionConfig", "InspectionResult",
+    "PixelScale", "PreprocessConfig", "QualityConfig", "ReferenceConfig", "ReferenceResidual", "RegionMeasurement",
+    "RegistrationResult", "assess_image_quality", "calibrate_camera", "compare_to_reference", "confusion_from_labels",
+    "inspect_array", "inspect_image", "measure_regions", "register_ecc", "rectify_perspective",
 ]
