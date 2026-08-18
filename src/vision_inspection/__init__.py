3f8b5bb9fcd5ec8363a63e031e0ee3553c4e1a18
Vivek <268113948+VivekVRobotics@users.noreply.github.com>
2026-08-18 19:24:49 +0530

    Expose ChArUco calibration APIs

--- src/vision_inspection/__init__.py
+++ src/vision_inspection/__init__.py
@@ -2,7 +2,7 @@
 
 from .acquisition import Frame, TriggeredSequenceCamera
 from .benchmarks import InspectionBenchmark, summarize_benchmark
-from .calibration import CameraCalibration, PixelScale, calibrate_camera
+from .calibration import CameraCalibration, CharucoCalibration, PixelScale, calibrate_camera, calibrate_charuco
 from .evaluation import BinaryConfusion, confusion_from_labels
 from .flat_field import apply_flat_field, build_flat_field, smooth_flat_field
 from .golden import GoldenSample, load_golden_registry, save_golden_registry
@@ -16,12 +16,12 @@ from .registration import RegistrationResult, register_ecc, rectify_perspective
 from .uncertainty import MeasurementUncertainty, propagate_length_uncertainty
 
 __all__ = [
-    "BinaryConfusion", "CameraCalibration", "Defect", "Frame", "GRRResult", "GoldenSample",
+    "BinaryConfusion", "CameraCalibration", "CharucoCalibration", "Defect", "Frame", "GRRResult", "GoldenSample",
     "ImageQuality", "InspectionBenchmark", "InspectionConfig", "InspectionResult", "MeasurementUncertainty",
     "PixelScale", "PreprocessConfig", "QualityConfig", "ReferenceConfig", "ReferenceResidual",
     "RegionMeasurement", "RegistrationResult", "TriggeredSequenceCamera", "apply_flat_field",
-    "assess_image_quality", "build_flat_field", "calibrate_camera", "compare_to_reference", "confusion_from_labels",
-    "crossed_grr", "inspect_array", "inspect_image", "load_golden_registry", "measure_regions",
-    "process_drift", "propagate_length_uncertainty", "rectify_perspective", "register_ecc",
+    "assess_image_quality", "build_flat_field", "calibrate_camera", "calibrate_charuco", "compare_to_reference",
+    "confusion_from_labels", "crossed_grr", "inspect_array", "inspect_image", "load_golden_registry",
+    "measure_regions", "process_drift", "propagate_length_uncertainty", "rectify_perspective", "register_ecc",
     "save_golden_registry", "smooth_flat_field", "summarize_benchmark",
 ]
